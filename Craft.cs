ed7dab458bbec5a24e2736c47fe2876e665ffd3b
Аушахманов Ильяс Жанатович <@corp.itstep>
2019-01-17 13:17:18 +0600

    first commit

---
+++ Craft.cs
@@ -0,0 +1,49 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Minecraft
+//{
+//    class CraftBoards
+//    {
+//        private string Tree { get; set; }
+//        private string Boards { get; set; }
+        
+        
+
+//        private string Cobblestone { get; set; }
+      
+        
+//        public static string operator +(CraftBoards Tree1, CraftBoards Tree2)
+//        {
+
+//            return Tree1.Boards + Tree2.Boards;
+//        }
+
+        
+
+     
+
+//    }
+//    class CraftStick
+//    {
+//        private string Stick { get; set; }
+//        public static string operator +(CraftStick Boards1, CraftStick Boards2)
+//        {
+//            return Boards1.Stick + Boards2.Stick;
+//        }
+
+//    }
+//    class CraftPickaxe
+//    {
+//        private string Pickaxe { get; set; }
+//        public static string operator +(CraftPickaxe Cobblestone, CraftPickaxe Stick)
+//        {
+//            return  Cobblestone.Pickaxe + Stick.Pickaxe;
+//        }
+//    }
+
+
+//}
---
+++ CraftingTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft
+{
+    public class CraftingTable
+    {
+        
+
+        public void Show()
+        {
+
+           
+            Console.WriteLine( "______________________");
+            Console.WriteLine("|       |      |      |");
+            Console.WriteLine("|       |      |      |");
+            Console.WriteLine("|_______|______|______|");
+            Console.WriteLine("|       |      |      |");
+            Console.WriteLine("|       |      |      |");
+            Console.WriteLine("|_______|______|______|");
+            Console.WriteLine("|       |      |      |");
+            Console.WriteLine("|       |      |      |");
+            Console.WriteLine("|_______|______|______|");
+            Console.SetCursorPosition(3, 5);
+            Console.WriteLine("y");
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+      
+
+
+
+
+
+
+    }
+}
